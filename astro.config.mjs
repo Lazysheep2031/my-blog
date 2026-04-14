3dac41bac8b7dbd15b7295dad5982b9e127c4401
Lazysheep2031 <3240102869@zju.edu.cn>
2026-04-14 22:13:27 +0800

    change http

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://www.lazysheep2031.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
