23aa89e7661e771fdb3b2143f85947b71b9f48a1
Lazysheep2031 <3240102869@zju.edu.cn>
2026-06-04 11:59:57 +0800

    feat: add landing home page

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -5,7 +5,7 @@ import { LinkPreset, type NavBarLink } from "@/types/config";
 export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 	[LinkPreset.Home]: {
 		name: i18n(I18nKey.home),
-		url: "/",
+		url: "/blog/",
 	},
 	[LinkPreset.About]: {
 		name: i18n(I18nKey.about),
